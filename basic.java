53bc8befa0bc1dd6de282f11703d2d4e03c0fa83
Sumit Prajapati <162676130+sumit7444@users.noreply.github.com>
2025-09-18 00:53:44 +0530

    initial

---
+++ basic.java
@@ -0,0 +1,15 @@
+package lecture1;
+import java.util.*;
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 System.out.println("* * * * *");
+     System.out.println("      * ");
+     System.out.println("    *");
+     System.out.println("  *");
+     System.out.println("* * * * *");
+
+	}
+
+}
