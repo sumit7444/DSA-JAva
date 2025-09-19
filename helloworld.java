c333af3960b1aadd1dda0a2a59f676f6684e7265
Sumit Prajapati <162676130+sumit7444@users.noreply.github.com>
2025-09-19 08:48:18 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package lecture1;
+import java.util.*;
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	    System.out.print("world");
+	}
+
+}
