8f4a63e9ca7f54002cb0a6080b2d6f7410c7dca5
Sumit Prajapati <162676130+sumit7444@users.noreply.github.com>
2025-09-24 00:32:52 +0530

    natural number

---
+++ print_natural_number.java
@@ -0,0 +1,19 @@
+package lecture1;
+import java.util.*;
+public class print_natural_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Scanner scn = new Scanner(System.in);
+	        int n = scn.nextInt();
+
+	        int i = 1;
+	        while(i <= n) {
+	        	System.out.println(i);
+	        	i = i + 1;
+	        }
+		}
+
+	}
+
+
